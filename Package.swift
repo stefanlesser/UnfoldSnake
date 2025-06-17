ee7bcf63ab0ff8f94bfab7240578c3b53423fbb2
Stefan Lesser <mail@stefan-lesser.com>
2025-06-17 11:50:44 +0100

    Cleaned up package description.

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "UnfoldSnake",
     platforms: [.macOS("15")],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SnakeLib",
             targets: ["Snake"]),
@@ -16,18 +13,20 @@ let package = Package(
             targets: ["SwiftUISnakeMain"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Snake"),
+            name: "Snake"
+        ),
         .testTarget(
             name: "UnfoldSnakeTests",
-            dependencies: ["Snake"]),
+            dependencies: ["Snake"],
+        ),
         .target(
             name: "SwiftUISnake",
-            dependencies: ["Snake"]),
+            dependencies: ["Snake"],
+        ),
         .executableTarget(
             name: "SwiftUISnakeMain",
-            dependencies: ["SwiftUISnake"])
+            dependencies: ["SwiftUISnake"],
+        )
     ]
 )
